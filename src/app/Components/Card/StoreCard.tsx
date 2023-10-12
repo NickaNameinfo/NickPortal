d402365a84de5c2ca05d1a9162cd331dd9546588
sampath1102 <sampath.p@convergepoint.com>
2023-10-12 22:49:09 +0530

    stoecard

---
+++ src/app/Components/Card/StoreCard.tsx
@@ -0,0 +1,292 @@
+import * as React from "react";
+import "../style.scss";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  CardFooter,
+} from "@nextui-org/react";
+import {
+  IconCall,
+  IconMap,
+  IconMapRound,
+  IconStar,
+  IconstoreCardNext,
+  Iconwhatsup,
+} from "../Icons";
+export const StoreCard = () => {
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      <div className="w-100 shodow bg-gradient-to-r from-sky-600 from-11% via-green-10 via-10% to-green-400 to-95% ">
+        <Card className="ps-1 Storecard p-0  ">
+          <CardHeader className="pb-0 pt-3 ps-2 flex-col items-start">
+            <p className="p-0 font-semibold text-base cardHeader">
+              Company Name Tag
+            </p>
+          </CardHeader>
+          <CardBody className="overflow-visible pt-2 pb-3.5 ps-2 pe-1 ">
+            <div className="grid grid-cols-12">
+              <div className="col-span-5">
+                <Image
+                  alt="Card background"
+                  className="object-cover rounded-xl p-0 m-0"
+                  src="https://nextui.org/images/hero-card-complete.jpeg"
+                />
+              </div>
+              <div className="col-span-7 ps-2">
+                <h2 className="font-medium text-sm cardHeader">Open :</h2>
+                <p className="font-normal text-xs StortimingColor tracking-tight mt-1">
+                  09: 30am-10:00pm
+                </p>
+                <div className="flex items-center ">
+                  <p className="mt-1 ">
+                    <IconStar fill="#FF9900" />
+                  </p>
+                  <p className="font-normal text-sm mt-1 ps-2 StortimingColor">
+                    4.3
+                  </p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="flex mt-1 justify-between basis-9/12 pe-2 items-center">
+                    <p className="cursor-pointer">
+                      <Iconwhatsup fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconCall fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMap fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMapRound fill="#6942CB" />
+                    </p>
+                  </p>
+
+                  <div className="mt-1 basis-3/12 justify-center flex">
+                    <div className="iconbox flex items-center justify-center cursor-pointer">
+                      <IconstoreCardNext fill="#6942CB" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardBody>
+
+          <CardFooter className="p-0">
+            <div className="grid grid-cols-2">
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Products : 150
+              </p>
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Near By : 15 Km
+              </p>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-100 shodow bg-gradient-to-r from-sky-600 from-11% via-green-10 via-10% to-green-400 to-95% ">
+        <Card className="ps-1 Storecard p-0  ">
+          <CardHeader className="pb-0 pt-3 ps-2 flex-col items-start">
+            <p className="p-0 font-semibold text-base cardHeader">
+              Company Name Tag
+            </p>
+          </CardHeader>
+          <CardBody className="overflow-visible pt-2 pb-3.5 ps-2 pe-1 ">
+            <div className="grid grid-cols-12">
+              <div className="col-span-5">
+                <Image
+                  alt="Card background"
+                  className="object-cover rounded-xl p-0 m-0"
+                  src="https://nextui.org/images/hero-card-complete.jpeg"
+                />
+              </div>
+              <div className="col-span-7 ps-2">
+                <h2 className="font-medium text-sm cardHeader">Open :</h2>
+                <p className="font-normal text-xs StortimingColor tracking-tight mt-1">
+                  09: 30am-10:00pm
+                </p>
+                <div className="flex items-center ">
+                  <p className="mt-1 ">
+                    <IconStar fill="#FF9900" />
+                  </p>
+                  <p className="font-normal text-sm mt-1 ps-2 StortimingColor">
+                    4.3
+                  </p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="flex mt-1 justify-between basis-9/12 pe-2 items-center">
+                    <p className="cursor-pointer">
+                      <Iconwhatsup fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconCall fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMap fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMapRound fill="#6942CB" />
+                    </p>
+                  </p>
+
+                  <div className="mt-1 basis-3/12 justify-center flex">
+                    <div className="iconbox flex items-center justify-center cursor-pointer">
+                      <IconstoreCardNext fill="#6942CB" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardBody>
+
+          <CardFooter className="p-0">
+            <div className="grid grid-cols-2">
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Products : 150
+              </p>
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Near By : 15 Km
+              </p>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-100 shodow bg-gradient-to-r from-sky-600 from-11% via-green-10 via-10% to-green-400 to-95% ">
+        <Card className="ps-1 Storecard p-0  ">
+          <CardHeader className="pb-0 pt-3 ps-2 flex-col items-start">
+            <p className="p-0 font-semibold text-base cardHeader">
+              Company Name Tag
+            </p>
+          </CardHeader>
+          <CardBody className="overflow-visible pt-2 pb-3.5 ps-2 pe-1 ">
+            <div className="grid grid-cols-12">
+              <div className="col-span-5">
+                <Image
+                  alt="Card background"
+                  className="object-cover rounded-xl p-0 m-0"
+                  src="https://nextui.org/images/hero-card-complete.jpeg"
+                />
+              </div>
+              <div className="col-span-7 ps-2">
+                <h2 className="font-medium text-sm cardHeader">Open :</h2>
+                <p className="font-normal text-xs StortimingColor tracking-tight mt-1">
+                  09: 30am-10:00pm
+                </p>
+                <div className="flex items-center ">
+                  <p className="mt-1 ">
+                    <IconStar fill="#FF9900" />
+                  </p>
+                  <p className="font-normal text-sm mt-1 ps-2 StortimingColor">
+                    4.3
+                  </p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="flex mt-1 justify-between basis-9/12 pe-2 items-center">
+                    <p className="cursor-pointer">
+                      <Iconwhatsup fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconCall fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMap fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMapRound fill="#6942CB" />
+                    </p>
+                  </p>
+
+                  <div className="mt-1 basis-3/12 justify-center flex">
+                    <div className="iconbox flex items-center justify-center cursor-pointer">
+                      <IconstoreCardNext fill="#6942CB" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardBody>
+
+          <CardFooter className="p-0">
+            <div className="grid grid-cols-2">
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Products : 150
+              </p>
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Near By : 15 Km
+              </p>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-100 shodow bg-gradient-to-r from-sky-600 from-11% via-green-10 via-10% to-green-400 to-95% ">
+        <Card className="ps-1 Storecard p-0  ">
+          <CardHeader className="pb-0 pt-3 ps-2 flex-col items-start">
+            <p className="p-0 font-semibold text-base cardHeader">
+              Company Name Tag
+            </p>
+          </CardHeader>
+          <CardBody className="overflow-visible pt-2 pb-3.5 ps-2 pe-1 ">
+            <div className="grid grid-cols-12">
+              <div className="col-span-5">
+                <Image
+                  alt="Card background"
+                  className="object-cover rounded-xl p-0 m-0"
+                  src="https://nextui.org/images/hero-card-complete.jpeg"
+                />
+              </div>
+              <div className="col-span-7 ps-2">
+                <h2 className="font-medium text-sm cardHeader">Open :</h2>
+                <p className="font-normal text-xs StortimingColor tracking-tight mt-1">
+                  09: 30am-10:00pm
+                </p>
+                <div className="flex items-center ">
+                  <p className="mt-1 ">
+                    <IconStar fill="#FF9900" />
+                  </p>
+                  <p className="font-normal text-sm mt-1 ps-2 StortimingColor">
+                    4.3
+                  </p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="flex mt-1 justify-between basis-9/12 pe-2 items-center">
+                    <p className="cursor-pointer">
+                      <Iconwhatsup fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconCall fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMap fill="#6942CB" />
+                    </p>
+                    <p className="cursor-pointer">
+                      <IconMapRound fill="#6942CB" />
+                    </p>
+                  </p>
+
+                  <div className="mt-1 basis-3/12 justify-center flex">
+                    <div className="iconbox flex items-center justify-center cursor-pointer">
+                      <IconstoreCardNext fill="#6942CB" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardBody>
+
+          <CardFooter className="p-0">
+            <div className="grid grid-cols-2">
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Products : 150
+              </p>
+              <p className=" pb-3 ps-2 font-normal text-sm tracking-tight cartfootercolor">
+                Near By : 15 Km
+              </p>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+};
+export default StoreCard;
