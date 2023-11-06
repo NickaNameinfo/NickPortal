d794c1a82fc26f099fb2eef68d790c6e276900b1
sampath1102 <sampath.p@convergepoint.com>
2023-11-07 01:46:47 +0530

    carrdsand info

--- src/app/Components/Card/ProductView.tsx
+++ src/app/Components/Card/ProductView.tsx
@@ -1,14 +1,35 @@
 import * as React from "react";
 import "../style.scss";
-import { Card, CardBody, Image } from "@nextui-org/react";
+import { Card, CardBody, Image, useDisclosure } from "@nextui-org/react";
+import { StoreList } from "./StoreList";
 
 export const ProductViewCard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div className="grid grid-cols-4 gap-2 mt-1">
       <Card
+        onPress={() => onOpen()}
         className="Storecard py-2.5 px-2.5"
         isPressable
-        onPress={() => console.log("item pressed")}
+      >
+        <CardBody className="overflow-visible p-0 relative">
+          <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
+            10 Store
+          </span>
+          <Image
+            alt="Here no Image"
+            shadow="md"
+            width="100%"
+            radius="lg"
+            className="w-full object-cover min-h-[220px]"
+            src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+          />
+        </CardBody>
+      </Card>
+      <Card
+        onPress={() => onOpen()}
+        className="Storecard py-2.5 px-2.5"
+        isPressable
       >
         <CardBody className="overflow-visible p-0 relative">
           <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
@@ -27,7 +48,7 @@ export const ProductViewCard = () => {
       <Card
         className="Storecard py-2.5 px-2.5"
         isPressable
-        onPress={() => console.log("item pressed")}
+        onPress={() => onOpen()}
       >
         <CardBody className="overflow-visible p-0 relative">
           <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
@@ -46,7 +67,7 @@ export const ProductViewCard = () => {
       <Card
         className="Storecard py-2.5 px-2.5"
         isPressable
-        onPress={() => console.log("item pressed")}
+        onPress={() => onOpen()}
       >
         <CardBody className="overflow-visible p-0 relative">
           <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
@@ -62,25 +83,7 @@ export const ProductViewCard = () => {
           />
         </CardBody>
       </Card>
-      <Card
-        className="Storecard py-2.5 px-2.5"
-        isPressable
-        onPress={() => console.log("item pressed")}
-      >
-        <CardBody className="overflow-visible p-0 relative">
-          <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
-            10 Store
-          </span>
-          <Image
-            alt="Here no Image"
-            shadow="md"
-            width="100%"
-            radius="lg"
-            className="w-full object-cover min-h-[220px]"
-            src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
-          />
-        </CardBody>
-      </Card>
+      <StoreList isOpen={isOpen} onClose={onClose} />
     </div>
   );
 };
