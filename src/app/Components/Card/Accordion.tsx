7811fcd7810d54ab6c54954fb7e7d6f420faaf99
sampath1102 <sampath.p@convergepoint.com>
2023-11-24 02:37:09 +0530

    updateSamz

---
+++ src/app/Components/Card/Accordion.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { IconsEye } from "../Icons";
+// import {AnchorIcon} from './AnchorIcon';
+// import {MoonIcon} from './MoonIcon';
+// import {SunIcon} from './SunIcon';
+
+export default function AccordionComp() {
+  return (
+    <div className="flex w-72 bg-slate-500 items-center">
+      <div className="flex w-11/12 items-center justify-between">
+        <div className=" ">sam</div>
+        <div className="">icom</div>
+      </div>
+      <div className=" w-1/12">Ac</div>
+    </div>
+  );
+}
--- src/app/Components/Card/grocery.tsx
+++ src/app/Components/Card/grocery.tsx
@@ -1,14 +1,65 @@
 import * as React from "react";
 import "../style.scss";
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  useDisclosure,
+} from "@nextui-org/react";
 import StoreHeaderCard from "./StoreHeaderCard";
+import { BuyCard } from "./BuyCard";
 
 export const GroceryCard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <StoreHeaderCard />
       <div className="grid grid-cols-4 gap-2">
-        <Card className="Storecard pt-3.5 px-3">
+        <Card
+          isPressable
+          onPress={() => onOpen()}
+          className="Storecard pt-3.5 px-3 cursor-pointer"
+        >
+          <CardBody className="overflow-visible p-0 relative">
+            <span className="bg-slate-700 z-50 absolute text-white text-xs font-medium px-2.5 py-1 rounded-ss-xl rounded-ee-xl dark:bg-gray-700 dark:text-gray-300">
+              Cod Available
+            </span>
+            <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
+              15KMsdas
+            </span>
+            <Image
+              isZoomed
+              alt="Here no Image"
+              shadow="md"
+              width="100%"
+              radius="lg"
+              className="w-full object-cover max-h-[176px]"
+              src="https://app.requestly.io/delay/1000/https://nextui.org/images/hero-card-complete.jpeg"
+            />
+          </CardBody>
+          <CardFooter className="p-0">
+            <div className="grid grid-cols-1 w-full">
+              <div className="font-semibold mt-3 text-sm  TextMaincolor">
+                Apple1 - Shimla
+              </div>
+              <div className="font-normal text-sm mt-2  TextMaincolor">
+                4 pcs (Approx. 550 - 640 g)
+              </div>
+              <div className="w-full flex justify-between mt-3 mb-3">
+                <p className="font-normal text-sm  Pricecolor">120 Stocks</p>
+                <p className="font-normal text-sm  TextMaincolor">
+                  Rs : 124.34
+                </p>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+        <Card
+          isPressable
+          onPress={() => onOpen()}
+          className="Storecard pt-3.5 px-3"
+        >
           <CardBody className="overflow-visible p-0 relative">
             <span className="bg-slate-700 z-50 absolute text-white text-xs font-medium px-2.5 py-1 rounded-ss-xl rounded-ee-xl dark:bg-gray-700 dark:text-gray-300">
               Cod Available
@@ -43,7 +94,11 @@ export const GroceryCard = () => {
             </div>
           </CardFooter>
         </Card>
-        <Card className="Storecard pt-3.5 px-3">
+        <Card
+          isPressable
+          onPress={() => onOpen()}
+          className="Storecard pt-3.5 px-3"
+        >
           <CardBody className="overflow-visible p-0 relative">
             <span className="bg-slate-700 z-50 absolute text-white text-xs font-medium px-2.5 py-1 rounded-ss-xl rounded-ee-xl dark:bg-gray-700 dark:text-gray-300">
               Cod Available
@@ -78,42 +133,11 @@ export const GroceryCard = () => {
             </div>
           </CardFooter>
         </Card>
-        <Card className="Storecard pt-3.5 px-3">
-          <CardBody className="overflow-visible p-0 relative">
-            <span className="bg-slate-700 z-50 absolute text-white text-xs font-medium px-2.5 py-1 rounded-ss-xl rounded-ee-xl dark:bg-gray-700 dark:text-gray-300">
-              Cod Available
-            </span>
-            <span className="bg-slate-700 z-50 right-0 absolute text-white text-xs font-medium px-2.5 py-1 rounded-se-xl rounded-es-xl dark:bg-gray-700 dark:text-gray-300">
-              5KM
-            </span>
-            <Image
-              isZoomed
-              alt="Here no Image"
-              shadow="md"
-              width="100%"
-              radius="lg"
-              className="w-full object-cover max-h-[176px]"
-              src="https://app.requestly.io/delay/1000/https://nextui.org/images/hero-card-complete.jpeg"
-            />
-          </CardBody>
-          <CardFooter className="p-0">
-            <div className="grid grid-cols-1 w-full">
-              <div className="font-semibold mt-3 text-sm  TextMaincolor">
-                Apple1 - Shimla
-              </div>
-              <div className="font-normal text-sm mt-2  TextMaincolor">
-                4 pcs (Approx. 550 - 640 g)
-              </div>
-              <div className="w-full flex justify-between mt-3 mb-3">
-                <p className="font-normal text-sm  Pricecolor">120 Stocks</p>
-                <p className="font-normal text-sm  TextMaincolor">
-                  Rs : 124.34
-                </p>
-              </div>
-            </div>
-          </CardFooter>
-        </Card>
-        <Card className="Storecard pt-3.5 px-3">
+        <Card
+          isPressable
+          onPress={() => onOpen()}
+          className="Storecard pt-3.5 px-3"
+        >
           <CardBody className="overflow-visible p-0 relative">
             <span className="bg-slate-700 z-50 absolute text-white text-xs font-medium px-2.5 py-1 rounded-ss-xl rounded-ee-xl dark:bg-gray-700 dark:text-gray-300">
               Cod Available
@@ -149,6 +173,7 @@ export const GroceryCard = () => {
           </CardFooter>
         </Card>
       </div>
+      <BuyCard isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
